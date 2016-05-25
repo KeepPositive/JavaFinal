8c0d31744de812af2117aa6c9a0f6bc5de44ec63
Ian Swire <oversizedpenguin@gmail.com>
2016-05-25 14:03:01 -0400

    Update diceGame.java

--- diceGame.java
+++ diceGame.java
@@ -1,20 +1,21 @@
 package com;
 import java.util.Scanner;
 
+/**
+ * Created by swir2476 on 5/17/2016.
+ */
 public class diceGame
 {
     public static void main(String[] args)
     {
-        int intPlayer = 1, intTurnScore, intGameScore, intDie1, intDie2;
-        final int WINSCORE = 100, COMPUTERSCORELIMIT = 20;
+        int intPlayer = 1, intTurnScore, intGameScore, intDie1, intDie2, intEndScore = 0;
+        final int WINSCORE = 100, COMSCORELIMIT = 20;
         String str11 = "Both of your die rolled a one, you lose all the points.";
         String str1x = "One of your die rolled a one, you lose all the points this turn.";
         String strxx = "Your turn ended with a turn score of ";
 
         die[]Die = new die[2];
         player[]Player = new player[2];
-
-        Scanner in = new Scanner(System.in);
 
         for(int intCount = 0; intCount < 2; intCount++)//Creates the players
         {
@@ -23,10 +24,10 @@ public class diceGame
         }
 
         printRules();
-        
-        while(Player[0].getIntGameScore() != WINSCORE || Player[1].getIntGameScore() != WINSCORE)
+
+        while(intEndScore <= WINSCORE)
         {
-            if(intPlayer == 1)//User
+            if (intPlayer == 1)//User
             {
                 intGameScore = Player[1].getIntGameScore();
                 System.out.println("COMs current game score is " + intGameScore);
@@ -42,37 +43,69 @@ public class diceGame
                     System.out.println(intDie2 + " <- Die 2");
                     if(intDie1 == 1 && intDie2 == 1)//if both equal 1
                     {
-                        System.out.println("double 1    -------    DEBUG");
                         Player[0].setZeroGameScore();
                         Player[0].setZeroTurnScore();
+                        intTurn = 0;
                         intPlayer = endUserTurn(str11,Player[0].getIntTurnScore(), Player[0].getIntGameScore(), Player[0].intPlayer(intPlayer));
                     }else if (intDie1 == 1 || intDie2 == 1)//if only one equals 1
                     {
-                        System.out.println("single 1    -------    DEBUG");
                         Player[0].setZeroTurnScore();
+                        intTurn = 0;
                         intPlayer = endUserTurn(str1x,Player[0].getIntTurnScore(), Player[0].getIntGameScore(), Player[0].intPlayer(intPlayer));
                     } else//neither of them equal 1
                     {
-                        System.out.println("no 1    -------    DEBUG");
                         intTurnScore = Player[0].AddTurnScore(intDie1, intDie2);
                         System.out.println("Turn score: " + intTurnScore + "   Game: " + intGameScore);
                         intTurn = rollAgain();
                     }
                 }//end of while
-                intPlayer = endUserTurn(strxx,Player[0].getIntTurnScore(), Player[0].getIntGameScore(), Player[0].intPlayer(intPlayer));
-                Player[0].setZeroTurnScore();
+                if(intTurn == 2)
+                {
+                    intPlayer = endUserTurn(strxx,Player[0].getIntTurnScore(), Player[0].getIntGameScore(), Player[0].intPlayer(intPlayer));
+                    Player[0].setZeroTurnScore();
+                    intEndScore = Player[0].getIntGameScore();
+                }
             }else if(intPlayer == -1)//Computer
             {
-                while(Player[1].getIntTurnScore() < COMPUTERSCORELIMIT)
+                while(intPlayer == -1)
                 {
                     intDie1 = Die[0].getIntFaceValue();
                     intDie2 = Die[1].getIntFaceValue();
-                    Player[1].AddTurnScore(intDie1, intDie2);
-                }
-                System.out.println("The Computer's turn is over.");
-                intPlayer = Player[1].intPlayer(intPlayer);
-            }
-        }
+                    System.out.println(intDie1 + " <- Die 1 - Computer Roll");
+                    System.out.println(intDie2 + " <- Die 2 - Computer Roll");
+                    if(intDie1 == 1 && intDie2 == 1)//if both equal 1
+                    {
+                        Player[1].setZeroGameScore();
+                        Player[1].setZeroTurnScore();
+                        intPlayer = Player[1].intPlayer(intPlayer);
+                    }else if (intDie1 == 1 || intDie2 == 1)//if only one equals 1
+                    {
+                        Player[1].setZeroTurnScore();
+                        intPlayer = Player[1].intPlayer(intPlayer);
+                    } else//neither of them equal 1
+                    {
+                        Player[1].AddTurnScore(intDie1, intDie2);
+                    }
+                    System.out.println("--------------------------------");
+                    if(Player[1].getIntTurnScore() >= COMSCORELIMIT)
+                    {
+                        System.out.println("The Computer's score limit reached.");
+                        intEndScore = Player[1].getIntGameScore();
+                        Player[1].setZeroTurnScore();
+                        intPlayer = Player[1].intPlayer(intPlayer);
+                    }
+                }//end of while
+            }//end of com
+        }//end of while
+        System.out.println("The game is over!");
+        intGameScore = Player[1].getIntGameScore();
+        System.out.println("COMs current game score is " + intGameScore);
+        intGameScore = Player[0].getIntGameScore();
+        System.out.println("Your current game score is " + intGameScore);
+        if(Player[1].getIntGameScore() > Player[0].getIntGameScore())
+            System.out.println("The computer has won! Better luck next time!");
+        else
+            System.out.println("You won! Great Job!");
     }//end of main
 
     public static void printRules()//prints the rules for the game
@@ -83,6 +116,8 @@ public class diceGame
         System.out.println("If you roll two 1's, you lose all your points. You can turn the dice over at any time.");
         System.out.println("However, if you roll one or two 1's, you lose your turn.");
         System.out.println("I (the computer) play by the same rules, except I'll always turn over the dice when I've rolled 20 or more points in a single turn.");
+        System.out.println();
+        System.out.println("--------------------------------");
     }
 
     public static int endUserTurn(String string, int turnScore, int playerScore, int playerSwitch)//when the user's turn ends
